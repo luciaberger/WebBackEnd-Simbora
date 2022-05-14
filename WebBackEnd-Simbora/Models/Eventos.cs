39f46d0d5df4e68708f691f7fc81aa342acd9473
luciaberger <91221827+luciaberger@users.noreply.github.com>
2022-05-14 12:09:26 -0300

    feat: Adicionar o model Eventos

---
+++ WebBackEnd-Simbora/Models/Eventos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBackEnd_Simbora.Models
+{
+	[Table("Eventos")]
+	public class Eventos
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		[Required(ErrorMessage = "Obrigatório informar o nome!")]
+		public string Nome { get; set; }
+		[Required(ErrorMessage = "Obrigatório informar a Data!")]
+		public int Data { get; set; }
+		[Required(ErrorMessage = "Obrigatório informar o Horário!")]
+		public int Horario { get; set; }
+		[Required(ErrorMessage = "Obrigatório informar o local!")]
+		public string Local { get; set; }
+		public string MaisInformacoes { get; set; }
+
+	}
+}
